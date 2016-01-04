f430b6f2b42a3b3035312e62e26e54492c2fc6bf
ksss <co000ri@gmail.com>
2016-01-04 16:06:42 +0900

    Implemnt Invert{all?,any?,sort}

--- lib/enumerator/invert.rb
+++ lib/enumerator/invert.rb
@@ -15,8 +15,22 @@ class Enumerator
     def grep(pat)
       @obj - @obj.grep(pat)
     end
+
+    def all?(&block)
+      !@obj.all?(&block)
+    end
+
+    def any?(&block)
+      !@obj.any?(&block)
+    end
+
+    def sort(&block)
+      @obj.sort(&block).reverse
+    end
   end
 end
 
-# p [].methods.v.grep(/i/)
-# p [].methods.v.grep(/i/) == [].methods.grep_v(/i/)
+p [].methods.v.grep(/i/) == [].methods.grep_v(/i/)
+p [1,2,3].v.all?{|i| 2 < i} == !([1,2,3].all?{|i| 2 < i})
+p [1,2,3].v.any?{|i| 2 < i} == !([1,2,3].any?{|i| 2 < i})
+p [3,1,2].v.sort == [3,2,1]
